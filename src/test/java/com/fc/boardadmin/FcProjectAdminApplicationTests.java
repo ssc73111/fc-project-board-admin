110eecc8c9c12fa7da2f041fc90dadbaff6b6de9
my <pd73111@gmail.com>
2023-12-04 15:19:20 +0900

    #1-init-boot: Auto stash before merge of "feature/#1-init-boot" and "origin/feature/#1-init-boot"

---
+++ src/test/java/com/fc/boardadmin/FcProjectAdminApplicationTests.java
@@ -0,0 +1,15 @@
+package com.fc.boardadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+class FcProjectAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
